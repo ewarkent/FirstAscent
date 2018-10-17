d44eaef79b21030ac9abd70bba1049c52e97e0b9
Scott <sctan6688@gmail.com>
2018-10-16 22:17:52 -0700

    Protoype of project
    setting up firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+
+// remove keys before pushing updates to github
+// Initialize Firebase
+
+
+const config = process.env.NODE_ENV === 'production'
+    ? prodConfig
+    : devConfig;
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+}
+
+const auth = firebase.auth();
+
+export {
+    auth,
+};
