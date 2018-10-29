54b85523367b109ef84e6c8fbd1cdd81d1c44adc
Scott <sctan6688@gmail.com>
2018-10-29 00:48:08 -0700

    Landing page and home page, view submissions
    Can see post submissions on landing page and home page now. Could use a lot of formatting. Posts should link to complete post view.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import withAuthorization from './withAuthorization';
+import * as firebase from '../firebase/firebase.js';
 import { db } from '../firebase';
 
 class HomePage extends Component {
@@ -9,6 +10,7 @@ class HomePage extends Component {
 
     this.state = {
       users: null,
+      posts: null,
     };
   }
 
@@ -18,14 +20,32 @@ class HomePage extends Component {
     );
   }
 
+  componentWillMount(){
+    let postsRef = firebase.db.ref('posts');
+
+    postsRef.once('value').then(snapshot => {
+      console.log(snapshot.val())
+
+      this.setState({
+        posts: snapshot.val()
+      })
+    })
+  }
+
+  componentWillUnmount(){
+    // what to put here?
+  }
+
   render() {
     const { users } = this.state;
+    const { posts } = this.state;
     return (
       <div>
         <h1>Home</h1>
         <p>The Home Page is accessible by every signed in user.</p>
 
         { !!users && <UserList users={users} /> }
+        { !!posts && <PostList posts={posts} /> }
       </div>
     );
   }
@@ -38,6 +58,22 @@ const UserList = ({ users }) =>
 
     {Object.keys(users).map(key =>
       <div key={key}>{users[key].username}</div>
+    )}
+  </div>
+
+const PostList = ({ posts }) =>
+  <div>
+    <h2>List of Posts</h2>
+    <p>Posts are submitted from the link to form.</p>
+
+    {Object.keys(posts).map(key =>
+      <div key={key}>
+        {posts[key].title} <br />
+        {posts[key].poster} <br />
+        {posts[key].location} <br />
+        {posts[key].description}  <br />
+        <br />
+      </div>
     )}
   </div>
 
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,6 +1,64 @@
 import React, {Component} from 'react';
-import { db } from '../firebase';
+import { db } from '../firebase/firebase.js';
 
+class LandingPage extends Component{
+    constructor(){
+    super();
+
+    this.state = {
+      posts: null,
+    };
+  }
+
+  componentWillMount(){
+    let postsRef = db.ref('posts');
+
+    postsRef.once('value').then(snapshot => {
+      console.log(snapshot.val())
+
+      this.setState({
+        posts: snapshot.val()
+      })
+    })
+  }
+  
+  componentWillUnmount(){
+    // what to put here?
+  }
+
+  render() {
+    const { posts } = this.state;
+    return (
+      <div>
+        <h1>Landing Page</h1>
+        <p>The Landing Page is the first thing that people will see.
+          We should show some list of climbs.???
+        </p>
+
+        { !!posts && <PostList posts={posts} /> }
+      </div>
+    );
+  }
+
+}
+
+const PostList = ({ posts }) =>
+  <div>
+    <h2>List of Posts</h2>
+    <p>Posts are submitted from the link to form.</p>
+
+    {Object.keys(posts).map(key =>
+      <div key={key}>
+        {posts[key].title} <br />
+        {posts[key].poster} <br />
+        {posts[key].location} <br />
+        {posts[key].description}  <br />
+        <br />
+      </div>
+    )}
+  </div>
+
+/*
 const LandingPage = () =>
   <div>
     <h1>Landing Page</h1>
@@ -8,6 +66,7 @@ const LandingPage = () =>
       We should show some list of climbs.
     </p>
   </div>
+*/
 
 
 
