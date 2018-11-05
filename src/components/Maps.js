4af23ff53f80f6659430360bdb22581c4f38d40e
Minh Nguyen <mnguy148@ucsc.edu>
2018-11-05 12:31:41 -0800

    added maps.js

---
+++ src/components/Maps.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { GoogleApiWrapper, InfoWindow, Map, Marker } from 'google-maps-react';
+
+
+class GoogleMapsContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {}
+    }
+    // binding this to event-handler functions
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.onMapClick = this.onMapClick.bind(this);
+  }
+  onMarkerClick = (props, marker, e) => {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  }
+  onMapClick = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  }
+  render() {
+    const style = {
+      width: '50vw',
+      height: '350px',
+      'marginLeft': 'auto',
+      'marginRight': 'auto'
+    }
+    return (
+      <Map
+        item
+        xs = { 12 }
+        style = { style }
+        google = { this.props.google }
+        onClick = { this.onMapClick }
+        zoom = { 14 }
+        initialCenter = {{ lat: 37.746495, lng: -119.533156 }}
+      >
+        <Marker
+          onClick = { this.onMarkerClick }
+          title = { 'Changing Colors Garage' }
+          position = {{ lat: 37.746495, lng: -119.533156 }}
+          name = { 'Changing Colors Garage' }
+        />
+        <InfoWindow
+          marker = { this.state.activeMarker }
+          visible = { this.state.showingInfoWindow }
+        >
+          
+        </InfoWindow>
+      </Map>
+    );
+  }
+}
+export default GoogleApiWrapper({
+    apiKey: ('AIzaSyBh8l0rMk0Y1eZRYMLAIJSwCq3zRESqVw8')
+})(GoogleMapsContainer)
