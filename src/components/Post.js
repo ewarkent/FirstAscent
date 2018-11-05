446507e1e051d72b176d0874ee42f4f0d7f73202
Minh Nguyen <mnguy148@ucsc.edu>
2018-11-04 20:03:27 -0800

    retrieved post key from url

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,17 +1,59 @@
 import React, {Component} from 'react';
+
+import withAuthorization from './withAuthorization';
 import * as firebase from '../firebase/firebase.js';
 import { db } from '../firebase';
 
 import './css/Post.css';
 import { extend } from '@firebase/util';
+import { DataSnapshot } from '@firebase/database';
 
+class PostPage extends Component {
+  constructor(props) {
+    super(props);
 
-const PostPage = () =>
-  <div className='postpage'>
+    this.state = {
+      post: null,
+    };
+  }
+
+  componentWillMount(){
+    var postRef = firebase.db.ref('post');
+    //getting key from url
+    var postKey = window.location.pathname.replace('/post/','');
+    console.log('postKey=' +postKey)
+
+    postRef.orderByKey().on("child_added", function(data){
+      console.log('dataKey=' +data.key)
+      if (data.key == postKey){
+        var p = data.val();
+        console.log("found"+ data.key);
+        console.log("title: "+p.title)
+
+        this.setState({
+          post: data.val()
+        })
+      }
+    });
+  }
+
+  render() {
+    const {post} = this.state;
+    return (
+      <div>
+        {<PostDisplay post={post} /> }
+        //{console.log(post)}
+      </div>
+    );
+  }
+}
+
+const PostDisplay = ({post}) =>
+<div className='postpage'>
     <h1>Post Page</h1>
 
     <div className='route-climber-names'>
-      <div className='route'>BIGO ROCK</div>
+      <div className='route'>Title</div>
       <div className='climber'>FA: Ethan</div>
     </div>
 
@@ -34,7 +76,6 @@ const PostPage = () =>
     </div>
 
   </div>
-
   
 
 export default PostPage;
